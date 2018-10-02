33b28534242aceda82a2eec365d7d0865d369d60
Pavel Marchenko <pgmarchenko@gmail.com>
2018-10-02 17:26:34 +0300

    Initial Commit

---
+++ DeviceScreenViewController/DeviceScreenViewController.h
@@ -0,0 +1,19 @@
+//
+//  DeviceScreenViewController.h
+//  DeviceScreenViewController
+//
+//  Created by Pavel Marchenko on 10/2/18.
+//  Copyright © 2018 pgmarchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceScreenViewController.
+FOUNDATION_EXPORT double DeviceScreenViewControllerVersionNumber;
+
+//! Project version string for DeviceScreenViewController.
+FOUNDATION_EXPORT const unsigned char DeviceScreenViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceScreenViewController/PublicHeader.h>
+
+
